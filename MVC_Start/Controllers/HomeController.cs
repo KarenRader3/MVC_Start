1badd851c0c74906fab82f6d68ba135e739c938a
Karen Rader <KarenClough@gmail.com>
2021-06-01 17:54:00 -0400

    starting point for assignment

--- MVC_Start/Controllers/HomeController.cs
+++ MVC_Start/Controllers/HomeController.cs
@@ -19,55 +19,19 @@ namespace MVC_Start.Controllers
       return View();
     }
 
-    public IActionResult Contact()
+   
+    
+    public ViewResult Chart()
     {
-      GuestContact contact = new GuestContact();
-
-      contact.Name = "Manish Agrawal";
-      contact.Email = "magrawal@usf.edu";
-      contact.Phone = "813-974-6716";
-
-      
-      /* alternate syntax to initialize object 
-      GuestContact contact2 = new GuestContact
-      {
-        Name = "Manish Agrawal",
-        Email = "magrawal@usf.edu",
-        Phone = "813-974-6716"
-      };
-      */
-
-      //ViewData["Message"] = "Your contact page.";
-
-      return View(contact);
-    }
-
-    [HttpPost]
-    public IActionResult Contact(GuestContact contact)
-    {
-      return View(contact);
-    }
-
-    /// <summary>
-    /// Replicate the chart example in the JavaScript presentation
-    /// 
-    /// Typically LINQ and SQL return data as collections.
-    /// Hence we start the example by creating collections representing the x-axis labels and the y-axis values
-    /// However, chart.js expects data as a string, not as a collection.
-    ///   Hence we join the elements in the collections into strings in the view model
-    /// </summary>
-    /// <returns>View that will display the chart</returns>
-    public ViewResult DemoChart()
-    {
-      string[] ChartLabels = new string[] { "Africa", "Asia", "Europe", "Latin America", "North America" };
-      int[] ChartData = new int[] { 2478, 5267, 734, 784, 433 };
+      string[] ChartLabels = new string[] { "Biscuits", "Cream Puffs", "Popovers", "Quick Loaf Breads", "Yeast Bread" };
+      int[] ChartData = new int[] { 15,60,60,75,40 };
 
       ChartModel Model = new ChartModel
       {
         ChartType = "bar",
         Labels = String.Join(",", ChartLabels.Select(d => "'" + d + "'")),
         Data = String.Join(",", ChartData.Select(d => d)),
-        Title = "Predicted world population (millions) in 2050"
+        Title = "Common Bread Baking Times"
       };
 
       return View(Model);
